940697c9d3e9b2fde99280a80ef379801ad973a9
Emman Jay Dizon <emman@puttiapps.com>
2025-08-27 00:46:22 +0800

    essentials for sending email

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
+// Initialize EmailJS with your public key
 (function () {
-  emailjs.init("");
+  emailjs.init("QuI-PoaZnlJWN_S2_"); // replace with your real public key
 })();
 
+// Handle form submission
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (e) {
     e.preventDefault();
 
     emailjs
-      .send("", "", {
+      .send("service_kyxpfss", "template_6v6wx0g", {
         title: "Email from my Portfolio",
         from_name: document.getElementById("name").value,
         from_email: document.getElementById("email").value,
